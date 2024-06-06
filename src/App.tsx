4ab9e156515bb95d0b54ddd0c299492bcfc2dc71
TARUGU UMA MAHESWAR REDDY <113752346+spk-krishna567@users.noreply.github.com>
2024-06-07 01:06:26 +0530

    warnings removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Tablelist } from "./components/Table";
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,7 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { CircularProgress, TextField } from "@mui/material";
+import { CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
